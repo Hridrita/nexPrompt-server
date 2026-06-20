6a870a25a66fdc37065035682faaa69c7bc56f2f
Hridrita <moonsaha721@gmail.com>
2026-06-20 23:42:10 +0600

    created prompt edit and delete api

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const cors = require("cors");
 const app = express();
 dotenv.config();
 
+app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = process.env.MONGO_URI;
 PORT = process.env.PORT;
 
@@ -27,10 +28,63 @@ async function run() {
     await client.connect();
 
     const db = client.db("nexPrompt_db");
+    const promptCollection = db.collection('prompts');
+
+    app.get('/api/prompts', async(req,res)=>{
+      const result = await promptCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post('/api/prompts', async(req,res) =>{
+      const prompt = req.body;
+      const newPrompt = {
+        ...prompt,
+        createdAt: new Date()
+      }
+
+      const result = await promptCollection.insertOne(newPrompt);
+      res.json(result);
+    });
+
+    app.patch('/api/prompts/:id', async(req,res)=>{
+      const id = req.params.id;
+      const updatedData = req.body;
+
+      const result = await promptCollection.updateOne(
+        {_id: new ObjectId(id)},
+        {$set: updatedData}
+      );
+      res.send(result);
+    });
+
+    app.delete('/api/prompts/:id', async(req,res)=>{
+      const id = req.params.id;
+      const result = await promptCollection.deleteOne({
+        _id: new ObjectId(id)
+      })
+      res.send(result);
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
