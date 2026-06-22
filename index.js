b8ea5f20b74db2f56300f6f72c7c4e263097ac5c
Hridrita <moonsaha721@gmail.com>
2026-06-22 22:50:25 +0600

    created bookmark save and unsave api with bookmark count

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = process.env.MONGO_URI;
 PORT = process.env.PORT;
 
@@ -20,7 +20,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,51 +28,56 @@ async function run() {
     await client.connect();
 
     const db = client.db("nexPrompt_db");
-    const promptCollection = db.collection('prompts');
+    const promptCollection = db.collection("prompts");
     const userCollection = db.collection("user");
+    const bookmarkCollection = db.collection("bookmark");
 
-    app.get('/api/prompts', async(req,res)=>{
+    //prompt related api's
+
+    app.get("/api/prompts", async (req, res) => {
       const result = await promptCollection.find().toArray();
       res.send(result);
     });
 
-    app.post('/api/prompts', async(req,res) =>{
+    app.post("/api/prompts", async (req, res) => {
       const prompt = req.body;
       const newPrompt = {
         ...prompt,
-        createdAt: new Date()
-      }
+        createdAt: new Date(),
+      };
 
       const result = await promptCollection.insertOne(newPrompt);
       res.json(result);
     });
 
-    app.patch('/api/prompts/:id', async(req,res)=>{
+    app.patch("/api/prompts/:id", async (req, res) => {
       const id = req.params.id;
       const updatedData = req.body;
 
       const result = await promptCollection.updateOne(
-        {_id: new ObjectId(id)},
-        {$set: updatedData}
+        { _id: new ObjectId(id) },
+        { $set: updatedData },
       );
       res.send(result);
     });
 
-    app.delete('/api/prompts/:id', async(req,res)=>{
+    app.delete("/api/prompts/:id", async (req, res) => {
       const id = req.params.id;
       const result = await promptCollection.deleteOne({
-        _id: new ObjectId(id)
-      })
+        _id: new ObjectId(id),
+      });
       res.send(result);
     });
 
-    app.get('/api/prompts/creator/:userId', async(req,res)=>{
+    app.get("/api/prompts/creator/:userId", async (req, res) => {
       const userId = req.params.userId;
-      const result = await promptCollection.find({creatorsId: userId}).toArray();
+      const result = await promptCollection
+        .find({ creatorsId: userId })
+        .toArray();
       res.send(result);
     });
 
-    app.get('/api/prompts/:id', async(req,res)=>{
+    app.get("/api/prompts/:id", async (req, res) => {
       const id = req.params.id;
       const query = {
         _id: new ObjectId(id),
@@ -80,15 +85,57 @@ async function run() {
       const prompt = await promptCollection.findOne(query);
 
       const creator = await userCollection.findOne({
-        _id: new ObjectId(prompt.creatorsId)
+        _id: new ObjectId(prompt.creatorsId),
       });
-      console.log('creator', creator);
+      console.log("creator", creator);
 
-      
-       
-      
-      res.send({...prompt, creator});
-    })
+      res.send({ ...prompt, creator });
+    });
+
+    //bookmark related api's
+
+    app.post("/api/bookmark", async (req, res) => {
+      const bookmarkData = req.body;
+      const result = await bookmarkCollection.insertOne(bookmarkData);
+      res.send(result);
+    });
+
+    app.patch("/api/prompts/:id/bookmark", async (req, res) => {
+      const id = req.params.id;
+      const result = await promptCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $inc: { bookmarkCount: 1 } },
+      );
+      res.send(result);
+    });
+
+    app.delete("/api/bookmark/remove", async (req, res) => {
+      const { promptId, userId } = req.body;
+      const result = await bookmarkCollection.deleteOne({ promptId, userId });
+      res.send(result);
+    });
+
+    app.patch("/api/prompts/:id/bookmark/decrement", async (req, res) => {
+      const id = req.params.id;
+      const result = await promptCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $inc: { bookmarkCount: -1 } },
+      );
+      res.send(result);
+    });
+
+    app.get("/api/bookmark/check", async (req, res) => {
+      const { userId, promptId } = req.query;
+      const existing = await bookmarkCollection.findOne({ userId, promptId });
+      res.json({ bookmarked: !!existing });
+    });
+
+    app.get('/api/bookmark/user/:userId', async(req,res)=>{
+      const userId = req.params.userId;
+      const result = await bookmarkCollection.find({userId:userId}).toArray();
+      res.send(result)
+
+    });
 
 
 
@@ -107,7 +154,9 @@ async function run() {
 
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
   } finally {
     // await client.close();
   }
